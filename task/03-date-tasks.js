c272a39dc9b83d21b8c49a60f6f9c2ff50c34206
RuslanZimnitski <105242067+RuslanZim@users.noreply.github.com>
2022-05-11 18:39:48 +0300

    Update 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 
@@ -56,7 +56,13 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+    console.log(year)
+    if (year % 400 === 0 || year%100 !==0 && (year % 4 ===0)){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
@@ -76,7 +82,14 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let date = new Date(endDate - startDate);
+   let hour = date.getUTCHours() > 24 ? date.getUTCHours() - 24 : date.getUTCHours(); 
+   let str = date.toISOString().slice(13, -1);
+   if(date.getDay()>4)
+       hour = `${hour + 24}`;
+   if(hour < 10)
+       hour = `0${hour}`
+   return `${hour}${str}`;
 }
 
 
@@ -94,7 +107,8 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   const angle =  0.5*Math.abs(60*(date.getUTCHours()%12)-11*(date.getUTCMinutes()%60));
+   return (angle <= 180 ? angle : 360 - angle)*Math.PI/180;
 }
 
 
